90f438a8196586852dba2907a6c54a46ef72b010
Linch <dr.linch19@gmail.com>
2024-01-07 11:57:46 +0300

    Create Client model

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Client(models.Model):
+    email = models.EmailField(verbose_name='Почта')
+    full_name = models.CharField(max_length=150, verbose_name='ФИО')
+    comment = models.TextField(verbose_name='Комментарий')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE,
+                              verbose_name='Менеджер клиента')
+
+    def __str__(self):
+        return f'{self.full_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
